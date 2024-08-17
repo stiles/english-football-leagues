7bf58eb1d620e301fc70e34a9270bca61a3b713c
Matt Stiles <mattstiles@gmail.com>
2024-08-16 23:05:47 -0700

    cleaning up fetch script

--- fetch_current_table.py
+++ fetch_current_table.py
@@ -2,14 +2,11 @@ import pandas as pd
 from datetime import datetime
 
 # Dates/times (UTC)
-timestamp = pd.Timestamp.today()
 week_number = datetime.now().isocalendar().week
-current_year = int(pd.Timestamp.today().strftime('%Y'))
+current_year = datetime.now().year
 next_year = current_year + 1
-season = f"{str(current_year)}-{str(next_year)}"
+season = f"{current_year}-{next_year}"
 today = pd.Timestamp.today().strftime('%Y-%m-%d')
-fetched = timestamp.strftime('%Y-%d-%m %H:%M:%S')
-fetched_str = timestamp.strftime('%-I %p, %B %d').replace('AM', 'a.m.').replace('PM', 'p.m.')
 
 # Outputs
 JSON_OUT = 'data/table/epl_table_latest.json'
@@ -18,9 +15,6 @@ JSON_SNAPSHOT_OUT = f'data/table/season_snapshots/epl_table_latest_year_{current
 CSV_SNAPSHOT_OUT = f'data/table/season_snapshots/epl_table_latest_year_{current_year}_week_{week_number}.csv'
 
 # Define url
-current_year = datetime.now().year
-next_year = current_year + 1
-season = f"{current_year}-{next_year}"
 url = f"https://fbref.com/en/comps/9/{season}/{season}-Premier-League-Stats"
 
 # Fetch table
@@ -34,6 +28,7 @@ df.columns = df.columns.str.lower().str.replace(' ', '_').str.replace('/', '_')
 df['squad'] = df['squad'].str.replace('Utd', "United").str.replace("Nott'ham", "Nottingham")
 
 # Add week number before exporting
+df['fetched_date'] = today
 df['week_no'] = week_number
 
 # Export csv, json files
